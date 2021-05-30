c6f3274077e4a4060576af195f4a8f0eeef87652
Ram <80225366+ramadhani892@users.noreply.github.com>
2021-05-30 17:54:53 +0700

    Create help.py

---
+++ userbot/help.py
@@ -0,0 +1,42 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+""" Userbot help command """
+
+import asyncio
+from userbot import ALIVE_NAME, CMD_HELP
+from userbot.events import register
+from platform import uname
+
+modules = CMD_HELP
+
+# ================= CONSTANT =================
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+# ============================================
+
+
+@register(outgoing=True, pattern="^.help(?: |$)(.*)")
+async def help(event):
+    """ For .help command,"""
+    args = event.pattern_match.group(1).lower()
+    if args:
+        if args in CMD_HELP:
+            await event.edit(str(CMD_HELP[args]))
+        else:
+            await event.edit("**`NGETIK YANG BENER NGENTOT!`**")
+            await asyncio.sleep(200)
+            await event.delete()
+    else:
+        string = ""
+        for i in CMD_HELP:
+            string += "`" + str(i)
+            string += "`\t 💀  "
+        await event.edit("**⭐Mekdi-UBOTT⭐**\n\n"
+                         f"**💀 Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**💀 Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
+                         "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
+                         f"💀 {string}\nNGETIK YANG BENER YA NGENTOOOOT!!\n\n")
+        await event.reply(f"\n**Contoh** : Ketik <`.help ping`> Untuk Informasi Pengunaan.\nJangan Lupa Berdoa Sebelum Mencoba wahahaha..")
+        await asyncio.sleep(50)
+        await event.delete()
